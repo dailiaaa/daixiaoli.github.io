7deb9f11f2522d596b730d75cccb05b9a19f8b9a
once <863122294@qq.com>
2017-07-22 22:56:01 +0800

    first commit

---
+++ personal website/js/index.js
@@ -0,0 +1,59 @@
+var menuItem = document.getElementsByClassName('profile')[0];
+var menuList = document.getElementsByClassName('menu-list')[0].getElementsByTagName('a');
+var timer = {};
+for (var i = 0; i < menuList.length; i++) {
+	menuList[i].timer = {};
+	menuList[i].addEventListener('mouseenter', function() {
+		var that = this;
+		clearInterval(that.timer.goDown);
+		
+		var hasMoved = parseFloat(window.getComputedStyle(that).top);
+		that.timer.goUp = setInterval(function() {
+			if (hasMoved < 150) {
+				hasMoved += 4;
+			}
+			that.style.top = '-' + hasMoved + 'px';
+		}, 1000/60);
+	}, false);
+	menuList[i].addEventListener('mouseleave', function() {
+		var that = this;
+		var hasMoved = parseFloat(that.style.top);
+		clearInterval(that.timer.goUp);
+		that.timer.goDown = setInterval(function(){ 
+			if (hasMoved < 0) {
+				hasMoved += 4;
+			}else {
+				clearInterval(timer.goDown);
+			}
+			that.style.top = hasMoved + 'px';
+		}, 1000/60);
+	}, false);
+}
+
+
+
+// menuItem.addEventListener('mouseenter', function() {
+// 	clearInterval(timer.goDown);
+// 	var that = this;
+// 	var hasMoved = parseFloat(window.getComputedStyle(that).top);
+// 	timer.goUp = setInterval(function() {
+// 		if (hasMoved < 150) {
+// 			hasMoved += 3;
+// 		}
+// 		that.style.top = '-' + hasMoved + 'px';
+// 	}, 1000/60);
+// }, false);
+// menuItem.addEventListener('mouseleave', function() {
+// 	var that = this;
+// 	var hasMoved = parseFloat(that.style.top);
+// 	console.log(hasMoved);
+// 	clearInterval(timer.goUp);
+// 	timer.goDown = setInterval(function(){ 
+// 		if (hasMoved < 0) {
+// 			hasMoved += 3;
+// 		}else {
+// 			clearInterval(timer.goDown);
+// 		}
+// 		that.style.top = hasMoved + 'px';
+// 	}, 1000/60);
+// }, false);
